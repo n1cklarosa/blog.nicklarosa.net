e0aef062c387c2a54da3e2a79a6e7546110917e2
Nick La Rosa <nick@nicklarosa.net>
2020-08-08 16:14:22 +1000

    Testing deploy

--- src/components/header.js
+++ src/components/header.js
@@ -19,6 +19,7 @@ const Header = ({ siteTitle, menuItems }) => (
         padding: `1.45rem 1.0875rem`,
         display: "flex",
         justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
       <Link
