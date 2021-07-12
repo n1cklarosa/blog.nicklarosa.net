e5d48d2daedf6f164d837f5c94a0b14456a342d5
Nick La Rosa <nick@nicklarosa.net>
2021-07-12 21:43:33 +1000

    Fixed footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ const Layout = ({ location, title, children }) => {
       <div className="page-wrapper" data-is-root-path={isRootPath}>
         <Header location={location} title={title} />
         <main  className="container mx-auto px-8 mb-8">{children}</main>
-        <footer>
+        <footer  className="container mx-auto px-8 mb-8 text-center">
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
